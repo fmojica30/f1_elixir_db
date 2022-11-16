3f41e36d1e267413cef3db936c9b8be037bdf397
Fernando Mojica <fernmarmojica@gmail.com>
2022-11-15 21:54:37 -0600

    Initial loading into drivers table

---
+++ src/seed.js
@@ -0,0 +1,12 @@
+import mysql from "mysql";
+import { loadDriverInfo } from "./driverInfo.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL || "";
+const connection = mysql.createConnection(connectionString);
+connection.connect();
+
+
+loadDriverInfo(connection);
