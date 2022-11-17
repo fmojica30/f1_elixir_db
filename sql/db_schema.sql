5520971951c865d98f2bce5cff91f4cd9be1a140
Fernando Mojica <fernmarmojica@gmail.com>
2022-11-17 11:49:47 -0600

    Add database schema and pdf updated

--- sql/db_schema.sql
+++ sql/db_schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `driver` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `first_name` varchar(100),
-  `last_name` varchar(100),
+  `first_name` varchar(1000),
+  `last_name` varchar(1000),
   `start_year` year,
-  `nation` varchar(100),
+  `nation` varchar(1000),
   `active` boolean,
   `world_champion` boolean,
   `winner` boolean
@@ -11,9 +11,9 @@ CREATE TABLE `driver` (
 
 CREATE TABLE `constructor` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar2(1000),
+  `name` varchar(1000),
   `active` boolean,
-  `nation` varchar2(1000),
+  `nation` varchar(1000),
   `start_year` date,
   `winner` boolean,
   `world_champion` boolean
@@ -31,17 +31,17 @@ CREATE TABLE `constructor_driver_pairings` (
 
 CREATE TABLE `engine` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar2(1000),
-  `nation` varchar2(1000),
+  `name` varchar(1000),
+  `nation` varchar(1000),
   `start_year` year,
   `winner` boolean,
-  `world_champion` bolean
+  `world_champion` boolean
 );
 
 CREATE TABLE `grand_prix` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar2(1000),
-  `country` varchar2(1000),
+  `name` varchar(1000),
+  `country` varchar(1000),
   `start_year` year
 );
 
@@ -87,3 +87,4 @@ ALTER TABLE `driver_season_stats` ADD FOREIGN KEY (`driver`) REFERENCES `driver`
 ALTER TABLE `driver_season_stats` ADD FOREIGN KEY (`constructor`) REFERENCES `constructor` (`id`);
 
 ALTER TABLE `driver_season_stats` ADD FOREIGN KEY (`engine`) REFERENCES `engine` (`id`);
+
